676dc1ec69a7a83cfa4d90cf4e9e1eeadd103f56
anchan <62826339+toshiki-anraku@users.noreply.github.com>
2022-05-03 10:54:50 +0900

    CSS追加 InlineStyle,CSSModules,StyledJSX,styled components,Emotion

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { CssModules } from "./component/CssModules";
+import { InlineStyle } from "./component/InlineStyle";
+import { StyledJsx } from "./component/StyledJsx";
+import { StyledComponents } from "./component/StyledComponents";
+import { Emotion } from "./component/Emotion";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <InlineStyle />
+      <CssModules />
+      <StyledJsx />
+      <StyledComponents />
+      <Emotion />
     </div>
   );
 }
